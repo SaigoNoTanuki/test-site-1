6d8e8f9e2ea10081c5c1ec81d010748442e8fb33
SaigoNoTanuki <omg7x2@gmail.com>
2025-08-23 21:24:04 +0600

    BUTTONS!!!!!

--- js/homepage.js
+++ js/homepage.js
@@ -12,3 +12,19 @@ function typeWriterAnim() {
 }
 
 typeWriterAnim();
+
+const toChange = document.getElementById("textChange");
+const changeButton = document.getElementById("textChangerButton");
+
+const toChangeText = ["A Slightly Changed Text","A Very Changed Text","This Text has Been Changed","A SUUUUPERRRR Changed Text","Stawp Already","I'm taking away your button privilages"]
+
+let textIndex = 0;
+
+changeButton.addEventListener("click",() => {
+    toChange.innerText = toChangeText[textIndex];
+    textIndex = (textIndex + 1);
+    if(textIndex == toChangeText.length) {
+        changeButton.style.display = "none";
+        textIndex % toChangeText.length;
+    };
+});
